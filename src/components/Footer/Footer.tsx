aa1ed858c9589edabe6954bd1899bf151a8ed47a
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-02-11 21:19:12 +0530

    updated logo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,17 @@
 import Container from '@/components/Container';
+import { Linkedin } from 'lucide-react';
+import Link from 'next/link';
 
-const Footer = () => {
+const Footer: React.FC = () => {
   return (
     <footer className="mt-20">
-      <Container className="p-6">
-        <p className="text-center text-zinc-500">
-          Built with <a className="underline font-medium text-inherit" href="https://cloudinary.com/">Cloudinary</a> &amp; <a className="underline font-medium text-inherit" href="https://nextjs.org/">Next.js</a> by <a className="underline font-medium text-inherit" href="https://twitter.com/colbyfayock">Colby Fayock</a>
+      <Container className="p-6 flex flex-col items-center">
+        <p className="text-center text-zinc-500 opacity-70">
+          10-2-2025 All rights reserved || Built by Mahesh
         </p>
+        <Link href="https://www.linkedin.com/in/mahesh-pagrut" target="_blank" rel="noopener noreferrer">
+          <Linkedin className="w-6 h-6 text-zinc-500 mt-2" />
+        </Link>
       </Container>
     </footer>
   );
