52a74d14ea34e68ab9f89a5637836de29a10b6b4
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-02-12 03:07:24 +0530

    ui changes

--- src/components/SidebarLinks/SidebarLinks.tsx
+++ src/components/SidebarLinks/SidebarLinks.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation';
 
 import { cn } from '@/lib/utils';
 import { ReactNode } from 'react';
@@ -19,23 +19,29 @@ interface SidebarLinksProps {
 
 export default function SidebarLinks({ className, links }: SidebarLinksProps) {
   const activePath = usePathname();
+
   return (
     <ul className={cn('', className)}>
       {links.map(({ label, path, icon }) => {
         const isActive = path === activePath;
+
         return (
           <li key={`${label}-${path}`} className="mb-2">
             <Link
               className={cn(
-                'flex w-full items-center gap-3 px-8 py-2 md:rounded-r-lg',
-                isActive && 'text-white bg-blue-500'
-              )} href={path}
+                'flex w-full items-center gap-3 px-8 py-2 transition-all duration-300 md:radius-r-lg',
+                'rounded-lg overflow-hidden', // Ensures rounded corners apply
+                isActive 
+                  ? 'text-white bg-gradient-to-r from-[#7F00FF] via-[#3A86FF] to-[#FF6B6B] shadow-lg' 
+                  : 'text-gray-700 hover:bg-gray-200'
+              )}
+              href={path}
             >
-              { icon } { label }
+              {icon} {label}
             </Link>
           </li>
-        )
+        );
       })}
     </ul>
-  )
+  );
 }
